9e56587b6669c348b873359d4aee37f104e13d5e
osuperthiago <osuperthiago@gmail.com>
2023-08-02 19:21:16 -0400

    Getting back in class 90

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
+
+
+const galleryImages = [
+    {
+        src: "./assets/gallery/image1.jpg",
+        alt: "Thumbnail Image 1"
+    },
+    {
+        src: "./assets/gallery/image2.jpg",
+        alt: "Thumbnail Image 2"
+    },
+    {
+        src: "./assets/gallery/image3.jpg",
+        alt: "Thumbnail Image 3"
+    },
+];
+
 //Menu Section
+
+function menuHandler (){
 
 document.querySelector("#open-nav-menu").addEventListener("click", function(){
     document.querySelector("header nav .wrapper").classList.add("nav-open")
@@ -8,18 +27,30 @@ document.querySelector("#close-nav-menu").addEventListener("click", function(){
     document.querySelector("header nav .wrapper").classList.remove("nav-open")
 })
 
-//Greeting Section
+}
 
 function celciusToFahr(temperature){
     let farh = (temperature * 9/5) + 32
     return farh;
 }
 
-celciusToFahr(0);
-celciusToFahr(10);
+//Greeting Section
 
+function greetingHandler (){
 
-const greetingText = "Good Morning!!!!!!!!!!!!!";
+    let currentHour = new Date ().getHours();
+let greetingText;
+
+if (currentHour < 12){
+    greetingText = "Good Morning.";
+} else if (currentHour < 19){
+    greetingText = "Good Afternoon.";
+} else if (currentHour < 24){
+    greetingText = "Good Night.";
+} else {
+    greetingText = " Out of Time in the day, somehow.";
+}
+
 const weatherCondition = "rainsss";
 const userLocation = "New jersey";
 let temperature = 0.1717;
@@ -39,55 +70,65 @@ document.querySelector(".weather-group").addEventListener("click", function(e) {
 
 });
 
+
+}
+
 //Local Time Section
 
-setInterval(function(){
-    let localTime = new Date ();
-    document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2,"0");
-    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
-    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
-},1000);
+function clickHandler(){
+    setInterval(function(){
+        let localTime = new Date ();
+        document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2,"0");
+        document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
+        document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
+    },1000);
+   
+}
 
 // Gallery Section
 
-//src="./assets/gallery/image1.jpg" alt="Thumbnail Image 1"
+function galleryHandler(){
 
-const galleryImages = [
-    {
-        src: "./assets/gallery/image1.jpg",
-        alt: "Thumbnail Image 1"
-    },
-    {
-        src: "./assets/gallery/image2.jpg",
-        alt: "Thumbnail Image 2"
-    },
-    {
-        src: "./assets/gallery/image3.jpg",
-        alt: "Thumbnail Image 3"
-    },
-];
+    let mainImage = document.querySelector("#gallery > img");
+    let thumbnails = document.querySelector("#gallery .thumbnails");
+     
+    
+    mainImage.src = galleryImages[0].src;
+    mainImage.alt = galleryImages[0].alt;
+    
+    galleryImages.forEach(function(image,index){
+        let thumb = document.createElement("img");
+        thumb.src = image.src;
+        thumb.alt = image.alt;
+        thumb.dataset.arrayIndex = index;
+        thumb.dataset.setected = index === 0 ? true : false;
+    
+        thumb.addEventListener("click", function(e){
+            let selectecIndex = e.target.dataset.arrayIndex;
+            let selectedImage = galleryImages[selectecIndex];
+            mainImage.src = selectedImage.src;
+            mainImage.alt = selectedImage.alt;
+            thumbnails.querySelectorAll("img").forEach(function(img){
+                img.dataset.setected = false;
+            })
+            e.target.dataset.setected = true;
+        });
+    
+        thumbnails.appendChild(thumb);
+    });
 
-/* for (let i in galleryImages){
-    console.log(galleryImages[i]);
-} */
-
-let mainImage = document.querySelector("#gallery > img");
-let thumbnails = document.querySelector("#gallery .thumbnails");
- 
-
-mainImage.src = galleryImages[0].src;
-mainImage.alt = galleryImages[0].alt;
+}
 
 
-//<img src="./assets/gallery/image1.jpg"
-//alt="Thumbnail Image 1"
-//data-array-index="0" data-selected="true">
 
-galleryImages.forEach(function(image,index){
-    let thumb = document.createElement("img");
-    thumb.src = image.src;
-    thumb.alt = image.alt;
-    thumb.dataset.arrayIndex = index;
-    thumb.dataset.setected = index === 0 ? true : false;
-    thumbnails.appendChild(thumb);
-});
+//Page Load
+
+menuHandler();
+greetingHandler ();
+clickHandler();
+galleryHandler();
+
+
+
+
+
